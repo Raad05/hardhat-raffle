2dc2deb61dab61ab74191adffda0f63ebdfd06fa
Raad05 <ashabulraad@gmail.com>
2024-02-24 14:04:14 +0600

    smart contract barebone

---
+++ contracts/Raffle.sol
@@ -0,0 +1,24 @@
+/*
+Features:
+1. Enter the lottery by paying some eth
+2. Pick a random winner (verifiably random)
+3. Winner to be selected every X minutes -> completely automated
+4. Chainlink Oracles -> Randomness, Automated execution (Chainlink Keeper)
+
+Steps:
+1. Write code
+2. Refactor code for gas optimization
+*/
+
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.9;
+
+// custom errors
+
+contract Raffle {
+    // variables
+    // constructor
+    // setter functions
+    // getter functions
+    // modifiers
+}
