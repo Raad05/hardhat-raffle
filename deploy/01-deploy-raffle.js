8a826c2f382fa9b9179c55ca0c06d44666343abb
Raad05 <ashabulraad@gmail.com>
2024-03-10 03:42:23 +0600

    added vrfCoordinatorV2Mock for testing

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,8 +1,18 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const args = [];
+  let vrfCoordinatorV2Address;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContractFactory(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = await vrfCoordinatorV2Mock.address();
+  }
 
   const raffle = await deploy("Raffle", {
     from: deployer,
