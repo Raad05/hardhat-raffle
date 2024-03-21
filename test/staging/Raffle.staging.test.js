d43d043fa9667a8f1eff0da1880a9634b773c215
Raad05 <ashabulraad@gmail.com>
2024-03-21 23:14:07 +0600

    staging test initialization

---
+++ test/staging/Raffle.staging.test.js
@@ -0,0 +1,31 @@
+const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../../helper-hardhat-config");
+const { assert, expect } = require("chai");
+
+developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("Raffle Unit Test:", async function () {
+      let raffle,
+        vrfCoordinatorV2Mock,
+        entranceFee,
+        deployer,
+        interval,
+        accounts;
+      const chainId = network.config.chainId;
+
+      beforeEach(async function () {
+        accounts = await ethers.getSigners();
+        deployer = (await getNamedAccounts()).deployer;
+        await deployments.fixture("all");
+        raffle = await ethers.getContract("Raffle", deployer);
+        vrfCoordinatorV2Mock = await ethers.getContract(
+          "VRFCoordinatorV2Mock",
+          deployer
+        );
+        entranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
+      });
+    });
