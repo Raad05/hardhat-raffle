a4739b86cceb28f24e3579c96c276022a80be6cb
Raad05 <ashabulraad@gmail.com>
2024-03-21 23:25:12 +0600

    staging test continued 1

--- test/staging/Raffle.staging.test.js
+++ test/staging/Raffle.staging.test.js
@@ -8,24 +8,12 @@ const { assert, expect } = require("chai");
 developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle Unit Test:", async function () {
-      let raffle,
-        vrfCoordinatorV2Mock,
-        entranceFee,
-        deployer,
-        interval,
-        accounts;
-      const chainId = network.config.chainId;
+      let raffle, entranceFee, deployer, accounts;
 
       beforeEach(async function () {
         accounts = await ethers.getSigners();
         deployer = (await getNamedAccounts()).deployer;
-        await deployments.fixture("all");
         raffle = await ethers.getContract("Raffle", deployer);
-        vrfCoordinatorV2Mock = await ethers.getContract(
-          "VRFCoordinatorV2Mock",
-          deployer
-        );
         entranceFee = await raffle.getEntranceFee();
-        interval = await raffle.getInterval();
       });
     });
